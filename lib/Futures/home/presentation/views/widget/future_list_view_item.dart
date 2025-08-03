1b1198ecb2659baecb2235aab1c013da808393dc
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-03 17:07:39 +0300

    fix problems

--- lib/Futures/home/presentation/views/widget/future_list_view_item.dart
+++ lib/Futures/home/presentation/views/widget/future_list_view_item.dart
@@ -1,23 +1,39 @@
+import 'package:bookly_app/Futures/home/presentation/manger/future_books_cubit/future_book_cubit.dart';
 import 'package:bookly_app/Futures/home/presentation/views/widget/card_item.dart';
+import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FutureListViewItem extends StatelessWidget {
   const FutureListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: CardItem(),
+    return BlocBuilder<FutureBookCubit, FutureBookState>(
+      builder: (context, state) {
+        if (state is FutureBookSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CardItem(),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is FutureBookFailure) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            showSnakBar(context, message: state.errMessage);
+          });
+          return const SizedBox.shrink(); 
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const kprimaryColor = Color(0XFF100B20);
 const kTranstionDuration = Duration(microseconds: 250);
 const kFontFamily = 'PlayfairDisplay';
+
+void showSnakBar(BuildContext context, {required String message}) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+}
