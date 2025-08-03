e89ece4454e96701714326f3e7183e3876b2260f
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-03 16:38:15 +0300

    provide cubits above material app

--- lib/Futures/home/presentation/views/home_view.dart
+++ lib/Futures/home/presentation/views/home_view.dart
@@ -1,23 +1,11 @@
-import 'package:bookly_app/Futures/home/data/models/repos/home_repo_implement.dart';
-import 'package:bookly_app/Futures/home/presentation/manger/future_books_cubit/future_book_cubit.dart';
-import 'package:bookly_app/Futures/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/Futures/home/presentation/views/home_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => FutureBookCubit(HomeRepoImplement())),
-        BlocProvider(
-          create: (context) => NewestBooksCubit(HomeRepoImplement()),
-        ),
-      ],
-      child: Scaffold(body: HomeViewBody()),
-    );
+    return Scaffold(body: HomeViewBody());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bookly_app/Futures/home/data/models/repos/home_repo_implement.dart';
+import 'package:bookly_app/Futures/home/presentation/manger/future_books_cubit/future_book_cubit.dart';
+import 'package:bookly_app/Futures/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setup();
   runApp(const Bookly());
 }
 
@@ -11,10 +17,20 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaryColor),
+    return MultiBlocProvider(
+       providers: [
+        BlocProvider(
+          create: (context) => FutureBookCubit(getIt.get<HomeRepoImplement>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImplement>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaryColor),
+      ),
     );
   }
 }
