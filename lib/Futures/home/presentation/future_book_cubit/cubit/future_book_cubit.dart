38786d169c83c85dc375523970bc6e994d1dd659
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-03 15:27:30 +0300

    create futured books method

--- lib/Futures/home/presentation/future_book_cubit/cubit/future_book_cubit.dart
+++ lib/Futures/home/presentation/future_book_cubit/cubit/future_book_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Futures/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Futures/home/data/models/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'future_book_state.dart';
 
 class FutureBookCubit extends Cubit<FutureBookState> {
-  FutureBookCubit() : super(FutureBookInitial());
+  FutureBookCubit(this.homeRepo) : super(FutureBookInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFutureBooks() async {
+    emit(FutureBookLoading());
+    var result = await homeRepo.fetchFutureBooks();
+    result.fold(
+      (failure) {
+        emit(FutureBookFailure(failure.errMessage));
+      },
+      (books) {
+        emit(FutureBookSuccess(books));
+      },
+    );
+  }
 }
